9aca3f1fed96b0713774704c75570397d0c064cd
torquecoder <torquecode@gmail.com>
2017-07-02 19:25:21 +0530

    Distribute Candy added

---
+++ Greedy/Distribute Candy.cpp
@@ -0,0 +1,21 @@
+int Solution::candy(vector<int> &ratings) {
+    int n = ratings.size();
+	 if(n <= 1)
+		 return n;
+	 vector<int> num(n, 1);
+	 for (int i = 1; i < n; i++)
+	 {
+		 if(ratings[i] > ratings[i - 1])
+			 num[i] = num[i - 1] + 1;
+	 }
+	 for (int i= n - 1; i > 0; i--)
+	 {
+		 if(ratings[i - 1] > ratings[i])
+			 num[i - 1] = max(num[i] + 1, num[i - 1]);
+	 }
+	 int ans = 0;
+	 for (int i = 0; i < n; i++)
+		 ans += num[i];
+	 return ans;
+}
+
