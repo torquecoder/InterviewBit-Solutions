43a7fcf2302b80eb630dda2bb2e7b30cc93b6dd5
torquecoder <torquecode@gmail.com>
2017-07-10 18:35:12 +0530

    Max Product Subarray added

---
+++ Dynamic Programming/Max Product Subarray.cpp
@@ -0,0 +1,14 @@
+int Solution::maxProduct(const vector<int> &A) {
+    int n = A.size();
+    int r = A[0];
+    for (int i = 1, imax = r, imin = r; i < n; i++) 
+    {
+        if (A[i] < 0)
+            swap(imax, imin);
+        imax = max(A[i], imax * A[i]);
+        imin = min(A[i], imin * A[i]);
+        r = max(r, imax);
+    }
+    return r;
+}
+
